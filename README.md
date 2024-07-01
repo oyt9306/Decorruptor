11faa049ddc7ce9f95ff2cbdfe5ee291f90140e1
Daniel123 <41467632+oyt9306@users.noreply.github.com>
2024-07-01 23:19:06 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# Decorruptor
-ECCV2024
+Title : Efficient Diffusion-Driven Corruption Editor for Test-Time Adaptation (https://arxiv.org/pdf/2403.10911)
+Venue: ECCV 2024
+# Abstract
+Test-time adaptation (TTA) addresses the unforeseen distribution shifts occurring during test time. In TTA, both performance and, memory and time consumption serve as crucial considerations. A recent diffusion-based TTA approach for restoring corrupted images involves image-level updates. However, using pixel space diffusion significantly increases resource requirements compared to conventional model updating TTA approaches, revealing limitations as a TTA method. To address this, we propose a novel TTA method by leveraging a latent diffusion model (LDM) based image editing model and fine-tuning it with our newly introduced corruption modeling scheme. This scheme enhances the robustness of the diffusion model against distribution shifts by creating (clean, corrupted) image pairs and fine-tuning the model to edit corrupted images into clean ones. Moreover, we introduce a distilled variant to accelerate the model for corruption editing using only 4 network function evaluations (NFEs). We extensively validated our method across various architectures and datasets including image and video domains. Our model achieves the best performance with a 100 times faster runtime than that of a diffusion-based baseline. Furthermore, it outpaces the speed of the model updating TTA method based on data augmentation threefold, rendering an image-level updating approach more practical.
+
+# Descriptions
+In this , we present further provide
+1) corruption modeling codes used for our model training (see 'save_pixmix.ipynb'), 
+2) pre-trained Decorruptor-DPM/CM demo (see 'decorruptor_DPM.ipynb'  and decorruptor_CM.ipynb'), and
+3) video corruption editing code (see 'decorruptor_Video_gen.ipynb'). 
+
+# Setup
+Install requirements using Python 3.9 and CUDA >= 11.6
+Need to support Jupyter Notebook for demo
+```
+conda create -n decorruptor python=3.9 -y
+conda activate decorruptor
+pip install -r requirements.txt
+python -m ipykernel install --user --name decorruptor --display-name "decorruptor"
+```
+Then activate decorruptor Jupyter kernel and use it for model inference. 
+
+# Datasets
+In __assets__ folder, we provide several sample images and videos for demo. 
+- clean_images : set of counterparts of corrupted images
+- corrupt_images : corrupted images sampled from ImageNet-C, C-bar
+- pixmix_samples : fractals and feature visualizations
+- videos : corrupted videos sampled from UCF-101C
+
---
+++ input_transform.py
@@ -0,0 +1,84 @@
+import torch 
+import random
+import numpy as np 
+import torchvision 
+from PIL import Image
+from PIL import ImageFilter
+from torchvision import transforms
+import pixmix_utils as utils
+from torchvision.transforms.functional import to_tensor, to_pil_image
+
+
+class GaussianBlur(object):
+    """Gaussian blur augmentation in SimCLR https://arxiv.org/abs/2002.05709"""
+
+    def __init__(self, sigma=[.1, 2.]):
+        self.sigma = sigma
+
+    def __call__(self, x):
+        sigma = random.uniform(self.sigma[0], self.sigma[1])
+        x = x.filter(ImageFilter.GaussianBlur(radius=sigma))
+        return x
+
+Simsiam_transform = transforms.Compose([       
+                transforms.RandomApply([
+                    transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)  # not strengthened
+                ], p=0.8), 
+                transforms.RandomApply([GaussianBlur([.1, 6.5])], p=0.5),
+                transforms.ToTensor(),
+    ])
+
+## PIXMIX
+def augment_input(image, aug_severity=1):
+    aug_list = utils.augmentations_all 
+    op = np.random.choice(aug_list)
+    return op(image.copy(), aug_severity)
+
+def pixmix_ori(orig, mixing_pic, preprocess):
+    assert isinstance(orig, Image.Image)
+    assert isinstance(mixing_pic, Image.Image)
+    
+    k, beta = 4, 4
+    mixings = utils.mixings
+    tensorize = preprocess['tensorize']
+    if np.random.random() < 0.5:
+        mixed = tensorize(augment_input(orig))
+    else:
+        mixed = tensorize(orig)
+
+    for _ in range(np.random.randint(k + 1)):
+        if np.random.random() < 0.5:
+            aug_image_copy = tensorize(augment_input(orig))
+        else:
+            aug_image_copy = tensorize(mixing_pic)
+    
+        # print(mixed.size(), aug_image_copy.size())
+        mixed_op = np.random.choice(mixings)
+        mixed = mixed_op(mixed, aug_image_copy, beta)
+        mixed = torch.clip(mixed, 0, 1)
+    return mixed
+
+def pixmix(orig, mixing_pic, mixing_pic2, preprocess):
+    assert isinstance(orig, Image.Image)
+    assert isinstance(mixing_pic, Image.Image)
+    assert isinstance(mixing_pic2, Image.Image)
+    
+    k, beta = 4, 4
+    mixings = utils.mixings
+    tensorize = preprocess['tensorize']
+    
+    mixed = tensorize(augment_input(orig))
+
+    for _ in range(0,np.random.randint(k)+1):
+        if np.random.random() < 0.25:
+            aug_image_copy = tensorize(augment_input(orig))
+        elif np.random.random() < 0.5 or np.random.random() > 0.25:
+            aug_image_copy = tensorize(mixing_pic)
+        elif np.random.random() < 0.75 or np.random.random() > 0.5:
+            aug_image_copy = tensorize(mixing_pic2)
+        else:
+            aug_image_copy = Simsiam_transform(orig)
+        mixed_op = np.random.choice(mixings)
+        mixed = mixed_op(mixed, aug_image_copy, beta)
+        mixed = torch.clip(mixed, 0, 1)
+    return mixed
